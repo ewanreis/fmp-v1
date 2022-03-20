7dff30332bae5ea2aba94aa037c075b6b337863e
ewanreis <87541930+ewanreis@users.noreply.github.com>
2022-03-20 15:55:21 +0000

    Update
    Sprint Exhaustion
    Smoother Sprinting
    Fixed Animation Snapping
    Code Optimisation + Readability

--- fmp/Assets/Scripts/playerController.cs
+++ fmp/Assets/Scripts/playerController.cs
@@ -1,17 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 public class playerController : MonoBehaviour
 {
     public CharacterController controller;
     public Animator playerAnimator;
+    public Slider staminaBar, healthBar;
     public Transform cam, groundCheck, playerBody;
     public LayerMask groundMask;
-    public float speed = 4, gravity = -9.81f, jumpHeight = 3, turnSmoothTime = 0.1f, groundDistance = 0.4f, mouseSensitivity, playerStamina = 100;
+    public float turnSmoothTime = 0.1f, mouseSensitivity, playerStamina = 100, playerHealth = 100;
+
+    private const float gravity = -9.8f, jumpHeight = 3;
 
     private Vector2 movementInput;
     private Vector3 moveDir, velocity, direction;
-    private float xRotation, turnSmoothVelocity, targetAngle, angle, mouseInputX;
+    private float xRotation, turnSmoothVelocity, targetAngle, angle, mouseInputX, groundDistance = 0.4f, speed = 4;
     private int attackIndex = 1;
     private bool isGrounded, isCrouching, isAttacking, isWalking, isTurning, canMove;
 
@@ -19,45 +23,41 @@ public class playerController : MonoBehaviour
 
     void Update()
     {
-        isCrouching = (Input.GetKey(KeyCode.LeftControl)) ? true : false;
-        isAttacking = (Input.GetButtonDown("Fire1")) ? true : false;
+        GetPlayerInput();
         if (isAttacking) canMove = false;
-        if (Input.GetButtonDown("Fire2") == true) attackIndex++;
-        if (playerStamina < 100) playerStamina += (1 * Time.deltaTime);
-        mouseInputX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        if (playerStamina < 100) playerStamina += (3 * Time.deltaTime);
         xRotation += mouseInputX;
+        staminaBar.value = playerStamina;
+        healthBar.value = playerHealth;
+        print(playerStamina);
         SetAnimations();
     }
 
     public void StartAttack(int attack)
     {
-        switch (attack)
+        int staminaCost = attack switch
         {
-            case 1:
-            print("Swirling Chaos");
-            canMove = true;
-            break;
-            case 2:
-            print("Gravity Pull");
-            canMove = true;
-            break;
-            case 3:
-            print("Amplified Gravity");
-            canMove = true;
-            break;
-            case 4:
-            print("Black Hole");
-            canMove = true;
-            break;
-            case 5:
-            print("Galactic Pull");
-            canMove = true;
-            break;
-            case 6:
-            print("Gravity Well");
-            canMove = true;
-            break;
-        }
+            1 => 25,
+            2 => 10,
+            3 => 30,
+            4 => 50,
+            5 => 20,
+            6 => 60,
+            _ => 0
+        };
+        playerStamina -= staminaCost;
+        string printAttack = attack switch
+        {
+            1 => "Swirling Chaos",
+            2 => "Gravity Pull",
+            3 => "Amplified Gravity",
+            4 => "Black Hole",
+            5 => "Galactic Pull",
+            6 => "Gravity Well",
+            _ => "Error"
+        };
+        print(printAttack);
+        canMove = true;
     }
 
     void SetAnimations()
@@ -67,10 +67,22 @@ public class playerController : MonoBehaviour
         playerAnimator.SetFloat("horizontalMouse", mouseInputX);
         playerAnimator.SetBool("crouching", isCrouching);
         playerAnimator.SetBool("attacking", isAttacking);
-        playerAnimator.SetInteger("attackIndex", attackIndex);
         playerAnimator.SetBool("jumping", !isGrounded);
+        playerAnimator.SetInteger("attackIndex", attackIndex);
     }
 
+    void GetPlayerInput()
+    {
+        isCrouching = (Input.GetKey(KeyCode.LeftControl)) ? true : false;
+        isAttacking = (Input.GetButtonDown("Fire1")) ? true : false;
+        if (Input.GetButtonDown("Fire2") == true) attackIndex++;
+        movementInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        movementInput.x = (Input.GetAxisRaw("Horizontal") == 0) ? 0 : movementInput.x;
+        movementInput.y = (Input.GetAxisRaw("Vertical") == 0) ? 0 : movementInput.y;
+        mouseInputX = Input.GetAxis("Mouse X") * mouseSensitivity;
+    }
+
+    // Player Physics + Movement
     void FixedUpdate()
     {
         #region Gravity
@@ -79,26 +91,27 @@ public class playerController : MonoBehaviour
         #endregion // Applies Gravity value at constant rate
 
         #region Walking
-        movementInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         isWalking = (movementInput.x != 0 || movementInput.y != 0) ? true : false;
         direction = new Vector3(movementInput.x, 0f, movementInput.y).normalized;
+        if (playerStamina > 0)
+        {
+            if (Input.GetKey(KeyCode.LeftShift) && speed < 8) speed += 0.1f;
+            else if (!Input.GetKey(KeyCode.LeftShift) && speed > 4) speed -= 0.1f;
+        }
+        else if(speed > 4) speed -= 0.1f;
         if(direction.magnitude >= 0.1f && canMove)
         {
             targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            speed = (Input.GetKey(KeyCode.LeftShift)) ? 8 : 4;
-            if (speed > 4) playerStamina -= (3 * Time.deltaTime);
+            if (speed > 4) playerStamina -= (6 * Time.deltaTime);
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
         #endregion // Moves the player
 
         #region Turning
-        mouseInputX = Input.GetAxis("Mouse X") * mouseSensitivity;
-        xRotation += mouseInputX;
         playerBody.transform.rotation = Quaternion.Euler(0, xRotation, 0);
         isTurning = (mouseInputX == 0) ? false : true;
-        //print(mouseInputX);
         #endregion // Gets the input from the mouse and turns the player
 
          #region Jumping
