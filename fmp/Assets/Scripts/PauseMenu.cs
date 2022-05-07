49327ad105bfe83c5f4a021c7eb7a123845720c3
ewanreis <87541930+ewanreis@users.noreply.github.com>
2022-05-07 23:34:19 +0100

    Sound Effects + Updated Menu Scene
    Imported SFX
    Added a sound effect for opening in game pause and setting menu
    Tweaked the main menu scene for better performance and added a portal opening visual effect when the game starts

--- fmp/Assets/Scripts/PauseMenu.cs
+++ fmp/Assets/Scripts/PauseMenu.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 public class PauseMenu : MonoBehaviour
 {
     public GameObject pauseMenu;
+    public AudioClip[] pauseSounds;
+    public AudioSource vfxSource;
     public static bool isPaused;
 
     void Start() => pauseMenu.SetActive(false);
@@ -23,6 +26,7 @@ public class PauseMenu : MonoBehaviour
 
     public void PauseGame()
     {
+        vfxSource.PlayOneShot(pauseSounds[1]);
         pauseMenu.SetActive(true);
         Cursor.lockState = CursorLockMode.None;
         Time.timeScale = 0f;
@@ -31,12 +35,14 @@ public class PauseMenu : MonoBehaviour
 
     public void ResumeGame()
     {
+        vfxSource.PlayOneShot(pauseSounds[0]);
         pauseMenu.SetActive(false);
         Cursor.lockState = CursorLockMode.Locked;
         Time.timeScale = 1f;
         isPaused = false;
     }
-
+    public void OpenSubMenu() => vfxSource.PlayOneShot(pauseSounds[1]);
+    public void CloseSubMenu() => vfxSource.PlayOneShot(pauseSounds[0]);
     public void ReturnToMenu() => SceneManager.LoadScene(0); 
     public void QuitGame() => Application.Quit();
 }
